4f33df49244d4f01b82eb7e06e974fba6d7669a2
RoyKoren <roykoren111@gmail.com>
2023-10-18 10:10:36 +0300

    Name Selection

--- Assets/Game/Features/NameSelection/Scripts/NameSelectionScreenController.cs
+++ Assets/Game/Features/NameSelection/Scripts/NameSelectionScreenController.cs
@@ -1,11 +1,62 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.Features.NameSelection.Scripts
 {
     public class NameSelectionScreenController : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _currentPlayerNameText;
+        [SerializeField] private Button _changeNameButton;
+        
+        private string _currentPlayerName;
+        
         private void Awake()
         {
+            _changeNameButton.onClick.AddListener(ChangePlayerName);
+        }
+        
+        private void ChangePlayerName()
+        {
+#if UNITY_EDITOR
+            // todo: move to the next screen
+#elif UNITY_ANDROID || UNITY_IOS
+            ChangePlayerNameAsync().Forget();
+#endif
+        }
+        
+        private async UniTask ChangePlayerNameAsync()
+        {
+            var statuesToAwait = new List<TouchScreenKeyboard.Status>
+            {
+                TouchScreenKeyboard.Status.Done,
+                TouchScreenKeyboard.Status.Canceled,
+                TouchScreenKeyboard.Status.LostFocus
+            };
+            
+            var newName = await GetKeyboardInput(
+                _currentPlayerName, TouchScreenKeyboardType.Default, statuesToAwait);
+            _currentPlayerName = newName;
+            _currentPlayerNameText.text = newName;
+            
+            // todo: select gender
+            // todo: save user name in a global context & move to the next screen
+        }
+        
+        private async UniTask<string> GetKeyboardInput(string startingText,
+            TouchScreenKeyboardType keyboardType, List<TouchScreenKeyboard.Status> statusesToAwait)
+        {
+            var keyboard = TouchScreenKeyboard.Open(startingText, keyboardType);
+            await UniTask.WaitUntil(() => statusesToAwait.Contains(keyboard.status));
+            var textInputInKeyboard = keyboard.text;
+            return textInputInKeyboard;
+        }
+        
+        private void OnDestroy()
+        {
+            _changeNameButton.onClick.RemoveListener(ChangePlayerName);
         }
     }
 }
