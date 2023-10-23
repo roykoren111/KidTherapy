2727804f65fedd5a10f0d6a2e120b3abc8fa0de1
talmzip <89967688+talmzip@users.noreply.github.com>
2023-10-23 21:38:56 +0300

    Breathing, Tension and InnerTalk Flow

---
+++ Assets/Game/Features/InnerTalk/TrialManagerInnerTalk.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrialManagerInnerTalk : MonoBehaviour
+{
+    // Slide Character to the screen buttom
+
+    // spawn sentence
+
+    // wait for whole sentence to complete.
+
+    // trigger sentence disappear animation
+    // trigger character empowered animation.
+
+    
+    // span tap indicator  TODO: move to round
+
+    
+    // wait for tap on word then remove tap indicator
+
+    // wait until the whole sentence is marked.
+
+
+
+}
+
+[Serializable]
+public class TrialInnerTalk
+{
+    public InnerTalkWord[] Words;
+}
