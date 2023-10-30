009c7623facece352f1d9c68461aefd991406d70
talmzip <89967688+talmzip@users.noreply.github.com>
2023-10-30 09:43:14 +0200

    environment movement

--- Assets/Game/Environment/EnvironmentMovement.cs
+++ Assets/Game/Environment/EnvironmentMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnvironmentMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    private float _startingZ = -78f;
+    private float _endingZ = 259.3f;
     void Start()
     {
 
@@ -14,5 +16,11 @@ public class EnvironmentMovement : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(0, 0, speed);
+        if (transform.position.z > _endingZ)
+        {
+            Vector3 targetPosition = transform.position;
+            targetPosition.z = _startingZ;
+            transform.position = targetPosition;
+        }
     }
 }
